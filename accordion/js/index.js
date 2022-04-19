cf1b7256179ac89d37f78b120df5ac4d1d2d6edf
Assim <assim.s@techversantinfo.com>
2022-04-19 18:11:14 +0530

    fINAL

--- accordion/js/index.js
+++ accordion/js/index.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
     $("a").on("click", function() {
       if ($(this).hasClass("active")) {
         $(this).removeClass("active");
-        $(this).siblings(".content").stop(true).slideUp(500);
+        $(this).siblings(".content").stop().slideUp(500);
         $(this).children(".up").removeClass("sp col")
       } else {
         $("a").removeClass("active");
         $("a").children(".up").removeClass("sp col")
         $(this).addClass("active");
-        $(".content").stop(true).slideUp(500);
-        $(this).siblings(".content").stop(true).slideDown(500); 
+        $(".content").stop().slideUp(500);
+        $(this).siblings(".content").stop().slideDown(500); 
         $(this).children(".up").addClass("sp col") 
       }
     });
